5778945c03e10892d430f502c81b7d28dae52381
VimalPranav <vimalpranavs08@gmail.com>
2026-06-14 21:49:05 +0530

    netlify error debugging 1

--- Frontend/src/redux/constants.js
+++ Frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://movie-planner-backend.onrender.com";
 export const USERS_URL = "/api/v1/users";
 export const MOVIE_URL = "/api/v1/movies";
 export const UPLOAD_URL = "/api/v1/upload";
